f1e48d7252687a72ef7c62ed54de9f19cc6899ff
M M Kishore <kishoremeenakshi2001@gmail.com>
2021-06-06 07:57:49 +0530

    react-router-dom added and navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
+    <Navbar/>
     </div>
+    </BrowserRouter>
   );
 }
 
