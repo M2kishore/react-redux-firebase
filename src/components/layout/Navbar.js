5eb4714cbaad7bd57b2f8745efb346a3d4d06963
M M Kishore <kishoremeenakshi2001@gmail.com>
2021-06-06 08:30:17 +0530

    navbar introduced

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom"
+import SignedInLink from './SignedInLinks'
+import SignedOutLink from './SignedOutLinks'
 
 const Navbar = () => {
     return ( 
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="bramd-logo">MarioPlan</Link>
+                <SignedInLink />
+                <SignedOutLink />
             </div>
         </nav>
      );
